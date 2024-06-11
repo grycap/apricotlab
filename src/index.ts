596d58a163c119782d3e0d53abec3f846cfccb1c
antonio <asanchezc30@gmail.com>
2024-06-11 19:21:03 +0200

    Changed css names

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ import { DOMUtils } from '@jupyterlab/apputils';
 import { Widget } from '@lumino/widgets';
 import { ButtonExtension } from './buttons';
 
-const TOP_AREA_CSS_CLASS = 'apricot-TopArea';
-
 // Notifications for deployed infrastructures (https://github.com/jupyterlab/extension-examples/tree/main/notifications)
 // Signals to make widgets communicate with eachother (https://github.com/jupyterlab/extension-examples/tree/main/signals)
 // state to save and restore data saved in persistent state database (https://github.com/jupyterlab/extension-examples/tree/main/state)
@@ -20,17 +18,17 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'Advanced Platform for Reproducible Infrastructure in the Cloud via Open Tools.',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension apricot is activated!');
+    console.log('JupyterLab extension APRICOT is activated!');
 
     // Create the HTML content of the widget
     const node = document.createElement('div');
-    node.className = 'apricot-TopArea';
-    node.innerHTML = 'APRICOT <i class="icon"></i>';
+    node.className = 'apricot-top-area';
+    node.innerHTML = 'APRICOT';
 
     // Create the widget
     const widget = new Widget({ node });
     widget.id = DOMUtils.createDomID(); // Widgets must have an unique identifier
-    widget.addClass(TOP_AREA_CSS_CLASS);
+    widget.addClass('apricot-top-area');
 
     // Add the widget to the top area of JupyterLab interface
     app.shell.add(widget, 'top', { rank: 1000 });
