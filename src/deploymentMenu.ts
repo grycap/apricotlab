980fd5f08393851fc7410558c9ec28504e2a539b
antonio <asanchezc30@gmail.com>
2024-07-26 10:14:37 +0200

    linted deploymentMenu.ts

--- src/deploymentMenu.ts
+++ src/deploymentMenu.ts
@@ -495,7 +495,7 @@ async function deployIMCommand(
 ): Promise<string> {
   const pipeAuth = `${obj.infName}-auth-pipe`;
   const imClientPath = await getIMClientPath();
-  const templatePath = `$PWD/deployed-template.yaml`;
+  const templatePath = '$PWD/deployed-template.yaml';
 
   let cmd = `%%bash
             PWD=$(pwd)
