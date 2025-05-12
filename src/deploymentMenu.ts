8ce55bdd3d46db6ddf31a55fcab912bbcd867008
antonio <asanchezc30@gmail.com>
2025-05-12 12:41:19 +0200

    linted file

--- src/deploymentMenu.ts
+++ src/deploymentMenu.ts
@@ -485,7 +485,7 @@ async function selectImage(obj: IDeployInfo): Promise<string> {
   } else if (obj.deploymentType === 'OpenStack') {
     authContent += `username = ${obj.username}; password = ${obj.password}; tenant = ${obj.tenant}; auth_version = ${obj.authVersion}; domain = ${obj.domain}`;
   } else if (obj.deploymentType === 'EGI') {
-    authContent += ` vo = ${obj.vo} `;
+    authContent += ` vo = ${obj.vo}`;
   }
 
   const cmd = `%%bash
